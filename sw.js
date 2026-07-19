209b7db5871c780790654efb302a7b81d4fb49ed
salomaogersonneto-sketch <salomao.gersonneto@gmail.com>
2026-07-19 17:40:08 -0300

    App GersonGains v2 - PWA para iPhone

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "gersongains-v1";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+// Rede primeiro (pega atualizações), cai para cache quando offline
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((m) => m || caches.match("./index.html")))
+  );
+});
